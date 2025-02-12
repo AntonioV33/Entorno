232829e6c5d34118961f6ef6cfd506a1a763dd62
AntonioV33 <antoniovillalbarodriguez73@gmail.com>
2025-02-12 19:33:19 +0100

    Add files via upload

---
+++ mi-proyecto-maven/src/main/java/com/example/mi_proyecto_maven/App.java
@@ -0,0 +1,5 @@
+package com.example.mi_proyecto_maven;
+
+public class App {
+
+}
---
+++ mi-proyecto-maven/src/test/java/com/example/mi_proyecto_maven/MiProyectoMavenApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.mi_proyecto_maven;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MiProyectoMavenApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
